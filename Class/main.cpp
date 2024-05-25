d9fa36d6ed6dd12fcd14ae057df0e8efb2f57dcb
Sondinh2k3 <ledinhsonx1td@gmail.com>
2024-05-25 16:18:11 +0700

    Creat a new folder

---
+++ Class/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Diem{
+    private:
+        double x,y;
+    public:
+        void Khoitao(double xx, double yy);
+        void Nhap();
+        void Xuat();
+        void Dichuyen(double dx, double dy);
+};
+
+int main(){
+
+    return 0;
+}
