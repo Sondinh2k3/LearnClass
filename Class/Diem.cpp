e84509e0c1c5453644dbf15ce0ad82d0dea31515
Sondinh2k3 <ledinhsonx1td@gmail.com>
2024-05-26 07:57:34 +0700

    Khoi tao phuong thuc ben trong class

--- Class/Diem.cpp
+++ Class/Diem.cpp
@@ -6,7 +6,8 @@ class Diem {
     private:
         double x, y;
     public:
-        void Khoitao(double x, double y);
+        //Cách khởi tạo hay dùng hơn
+        Diem(double x = 0, double y = 0): x(x), y(y){};
         void Trungnhau(Diem u);
         double Khoangcach(Diem u);
         void Xuat();
@@ -15,10 +16,11 @@ void Diem::Xuat(){
     cout<<"("<<this->x<<", "<<this->y<<")\n";
 }
 
-void Diem::Khoitao(double x, double y){
-    this->x = x;
-    this->y = y;
-}
+// void Diem::Khoitao(double x, double y){
+//     this->x = x;
+//     this->y = y;
+// }
+
 
 double Diem::Khoangcach(Diem u){
     return sqrt(pow(u.x-this->x, 2) + pow(u.y-this->y, 2));
@@ -30,12 +32,14 @@ void Diem::Trungnhau(Diem u){
     }
 }
 int main(){
-    Diem a;
-    a.Khoitao(5,6);
+    Diem a(5,6);
+    // a.Khoitao(5,6);
     a.Xuat();
-    Diem b;
-    b.Khoitao(2,5);
+    Diem b(2,5);
+    // b.Khoitao(2,5);
     b.Xuat();
+
+    
     a.Trungnhau(b);
     cout<<a.Khoangcach(b);
     return 0;
